5d7a656e0b4c3c3f660d64885433d1b85df90591
Christine Godfrey <cldonnen2001@yahoo.com>
2021-05-17 19:45:00 -0500

    updating

--- CollectionsMasterConsoleUI/Program.cs
+++ CollectionsMasterConsoleUI/Program.cs
@@ -11,44 +11,43 @@ namespace CollectionsMasterConsoleUI
             //Make the console formatted to display each section well
             //Utlilize the method stubs at the bottom for the methods you must create
 
-            #region Arrays
-            // Create an integer Array of size 50
+           #region Arrays
+           var numbers = new int [50];                         //Create an integer Array of size 50
+
+           Console.WriteLine("Random numbers: ");
+              Populater(numbers);                              //Create a method to populate the number array with 50 random numbers that are between 0 and 50 - from below method
+                          
+           Console.WriteLine($"{numbers[0]}");                 //Print the first number of the array - this populates the first number in the random order sequence
+           Console.WriteLine($"{numbers[numbers.Length-1]}");  //Print the last number of the arrayy - this populates the last number in the random order sequence
             
+           Console.WriteLine("-------------------");
 
-            //Create a method to populate the number array with 50 random numbers that are between 0 and 50
+
+           Console.WriteLine("All Numbers Original");          //This is pulling from Random list which is ok, because steps request the array to be random
+              NumberPrinter(numbers);                          //call line Use this method to print out your numbers from arrays or lists - this populates all the numbers in the random sequence, 
+                                                               //NumberPrinter provided  - this is from last method at bottom of list
+
+           Console.WriteLine("-------------------");
+                                                 
+           
+           Console.WriteLine("All Numbers Reversed:");         //Reverse the contents of the array and then print the array out to the console.  Try for 2 different ways   Hint: Array._____(); Create a custom method        
+              ReverseArray(numbers);                           //call line
+           
+           Console.WriteLine("---------REVERSE CUSTOM------------");
             
+           
+           Console.WriteLine("Multiple of three = 0: ");       //Create a method that will set numbers that are a multiple of 3 to zero then print to the console all numbers
+              ThreeKiller(numbers);                            //call line
 
-            //Print the first number of the array
+           Console.WriteLine("-------------------");
 
-            //Print the last number of the array            
+                       
+           Console.WriteLine("Sorted numbers:");               //Sort the array in order now   /*      Hint: Array.____()      */
+              Array.Sort(numbers);                             //This occurs after the numbers divisible by 3, to those numbers are still 0
+              NumberPrinter(numbers);
+                
 
-            Console.WriteLine("All Numbers Original");
-            //Use this method to print out your numbers from arrays or lists
-            //NumberPrinter();
-            Console.WriteLine("-------------------");
-
-            //Reverse the contents of the array and then print the array out to the console.
-            //Try for 2 different ways
-            /*     Hint: Array._____(); Create a custom method     */
-
-            Console.WriteLine("All Numbers Reversed:");
-
-            Console.WriteLine("---------REVERSE CUSTOM------------");
-
-            Console.WriteLine("-------------------");
-
-            //Create a method that will set numbers that are a multiple of 3 to zero then print to the console all numbers
-            Console.WriteLine("Multiple of three = 0: ");
-            
-
-            Console.WriteLine("-------------------");
-
-            //Sort the array in order now
-            /*      Hint: Array.____()      */
-            Console.WriteLine("Sorted numbers:");
-            
-
-            Console.WriteLine("\n************End Arrays*************** \n");
+           Console.WriteLine("\n************End Arrays*************** \n");
             #endregion
 
             #region Lists
@@ -99,9 +98,16 @@ namespace CollectionsMasterConsoleUI
             #endregion
         }
 
-        private static void ThreeKiller(int[] numbers)
-        {
-            
+        private static void ThreeKiller(int[] numbers)         //Create a method that will set numbers that are a multiple of 3 to zero then print to the console all numbers
+        {  
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 3 == 0)
+                {
+                    numbers[i] = 0;
+                }
+            } 
+         NumberPrinter(numbers);
         }
 
         private static void OddKiller(List<int> numberList)
@@ -117,18 +123,25 @@ namespace CollectionsMasterConsoleUI
         private static void Populater(List<int> numberList)
         {
             Random rng = new Random();
-
         }
 
-        private static void Populater(int[] numbers)
+        private static void Populater(int[] numbers)    //Create a method to populate the number array with 50 random numbers that are between 0 and 50
         {
-            Random rng = new Random();
-
+            for (int i = 0; i < numbers.Length; i++)  // put value in the 50 spots
+            {
+                Random rng = new Random();
+                numbers [i] = rng.Next(0, 50);        //  (0,50) - this is the range
+            }
         }        
 
-        private static void ReverseArray(int[] array)
+        private static void ReverseArray(int[] array)   //Reverse the contents of the array and then print the array out to the console.
         {
-            
+           Array.Reverse(array);
+            //foreach (var number in array)
+            //{
+            //    Console.WriteLine($"{number}");
+            //}
+           NumberPrinter(array);  //  this will work the same as the foreach loop
         }
 
         /// <summary>
